f72f21821be675a66467a9188379283e6348b7df
Weslei França <weslwifranca@gmail.com>
2023-12-27 20:03:30 -0300

    new test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route} from 'react-router-dom'
 import { Home } from '../pages/Home'
-import { PrivateRoutes } from './PrivateRoutes'
-import { Dashboard } from '../pages/Dashboard'
+// import { PrivateRoutes } from './PrivateRoutes'
+// import { Dashboard } from '../pages/Dashboard'
 import { Login } from '../pages/Login'
 
 export const Router = () => {
@@ -10,9 +10,9 @@ export const Router = () => {
       <Route path='/' element={<Home/>} />
       <Route path='/login' element={<Login/>} />
     
-      <Route path="/dashboard" element={<PrivateRoutes />}>
-          <Route path="/" element={<Dashboard />} />
-      </Route>
+      {/* <Route path="/" element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+      </Route> */}
     </Routes>
   )
 }
