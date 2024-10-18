15a240b36515bab14763bda523ba0135d3994878
Weslei França <weslwifranca@gmail.com>
2024-10-18 15:38:50 -0300

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://is53.com.br', // substitua pelo domínio do seu site
+      dynamicRoutes: [
+        '/',
+        '/login',
+        '/atualizar-senha',
+        "/dashboard",
+      ],
+    }),
+  ],
 })
